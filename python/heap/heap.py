20414c98efc1c80942cf1bd28a9dec789baca8d9
Abhishek Kasireddy <akasireddy@commure.com>
2024-06-08 14:31:17 +0530

    fixes

--- python/heap/heap.py
+++ python/heap/heap.py
@@ -1,9 +1,3 @@
-# The Heap Property
-# The top of the tree is always the min
-
-# Note: This is a min-heap implementation
-
-
 class Heap:
     def __init__(self) -> None:
         self.heap = []
@@ -18,7 +12,6 @@ class Heap:
         return (2 * i) + 2
 
     def add(self, num):
-        # Add an element at the end of the array, and bubble up
         self.heap.append(num)
         self.up_heapify(len(self.heap) - 1)
 
@@ -34,26 +27,22 @@ class Heap:
         return min_element
 
     def up_heapify(self, i):
-        while i != 0 and self.heap[self.parent(i)] > self.heap[i]:
-            self.heap[self.parent(i)], self.heap[i] = (
-                self.heap[i],
-                self.heap[self.parent(i)],
+        current = i
+        while current != 0 and self.heap[self.parent(current)] > self.heap[current]:
+            self.heap[self.parent(current)], self.heap[current] = (
+                self.heap[current],
+                self.heap[self.parent(current)],
             )
-            self.up_heapify(self.parent(i))
+            current = self.parent(current)
 
     def down_heapify(self, i):
-        # when extracting a min from the top, we need to replace the root
-        # with the lowest element, and heapify_down, such that we can maintain
-        # the heap property
         smallest = i
         left = self.l_child(i)
         right = self.r_child(i)
         heap_size = len(self.heap)
 
-        # Check if left child exists and is smaller than top
         if left < heap_size and self.heap[left] < self.heap[smallest]:
             smallest = left
-
         if right < heap_size and self.heap[right] < self.heap[smallest]:
             smallest = right
 
@@ -62,6 +51,7 @@ class Heap:
             self.down_heapify(smallest)
 
 
+# Test the heap operations
 heap = Heap()
 heap.add(3)
 heap.add(2)
@@ -69,6 +59,8 @@ heap.add(15)
 heap.add(5)
 heap.add(4)
 heap.add(45)
+print(heap.heap)
+print(heap.extract())
 print(heap.heap)
 print(heap.extract())
 print(heap.heap)
